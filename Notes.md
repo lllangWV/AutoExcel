cd35b1de1db2fd2bcd9c418e94c8f81926f01390
lllangWV <lllangWV@gmail.com>
2024-12-03 15:01:49 -0500

    Removed setuptoll-scm

---
+++ Notes.md
@@ -0,0 +1,1 @@
+Downloading zip file from Git repository and trying to install the environment using conda env create -f env.yml fail setuptools scm failed
--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
-from setuptools_scm import ScmVersion
+# from setuptools_scm import ScmVersion
 
-def version_for_project(version: ScmVersion) -> str:
-   return str(version.tag)
+# def version_for_project(version: ScmVersion) -> str:
+#    return str(version.tag)
    
-setup(use_scm_version={'version_scheme':version_for_project})
+# setup(use_scm_version={'version_scheme':version_for_project})
+
+
+   
+setup()
