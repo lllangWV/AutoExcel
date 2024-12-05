17c485e3477c3618154aa58445487f33b5f7ab1c
lllangWV <lllangWV@gmail.com>
2024-12-05 17:21:48 -0500

    Updated the main method and the example

--- examples/script/Example 1 - FY Caseload Analysis.py
+++ examples/script/Example 1 - FY Caseload Analysis.py
@@ -2,17 +2,15 @@ from datetime import datetime
 from autoexcel.main import fy_analysis
 from autoexcel.utils.config import config
 import os
-import numpy as np
 
-# np.busday_count
-
-config.logging_config.loggers.autoexcel.level='DEBUG'
+config.logging_config.loggers.autoexcel.level='INFO'
 config.apply()
 
+old_negotiators = ['Abigail Gallagher', 'Eric Divito', 'Eric Winaught', 'Jillian Corbett', 'Huron – New']
 
 raw_dir = os.path.join(config.data_dir, 'fy_analysis_test', 'raw')
-template_dir = os.path.join(config.data_dir, 'fy_analysis_test', 'templates')
 processed_dir = os.path.join(config.data_dir, 'fy_analysis_test', 'processed')
+
 assigned_date_filter=[datetime(2023, 7, 1), None]
 
-fy_analysis(raw_dir, processed_dir, assigned_date_filter=assigned_date_filter)
+fy_analysis(raw_dir, processed_dir, assigned_date_filter=assigned_date_filter, old_negotiators=old_negotiators)
